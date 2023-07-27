93b263a70069abee66ee7c81bf48921251ca6192
pnk283 <naresh.p@tuitionworld.co.in>
2023-07-27 21:02:14 +0530

    new project

---
+++ src/main/java/com/example/model/CustomerDetails.java
@@ -0,0 +1,36 @@
+package com.example.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+
+import org.springframework.lang.Nullable;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+
+@Entity
+@Table(name = "CRM_PROJECT")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class CustomerDetails {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Integer id;
+	
+	@NotNull(message = "Firstname should not be null")
+	private String firstName;
+
+	@NotNull(message = "Lastname should not be null")
+	private String lastName;
+
+	@javax.validation.constraints.Email(message = "invalid email address")
+	private String Email;
+
+}
